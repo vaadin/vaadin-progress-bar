e6c1cd7ebd971afa5015642650a42d45bb44bdf4
Phan Hong Duc <phduc.phanhongduc@gmail.com>
2020-04-03 10:13:31 +0300

    refactor: convert theme and visual tests (#68)
    Co-authored-by: web-padawan <iamkulykov@gmail.com>

---
+++ theme/lumo/vaadin-progress-bar-styles.ts
@@ -0,0 +1,4 @@
+import { registerStyles } from '@vaadin/vaadin-themable-mixin/register-styles.js';
+import { progressBarStyles } from './vaadin-progress-bar-css';
+
+registerStyles('vaadin-progress-bar', progressBarStyles, { moduleId: 'lumo-progress-bar' });
